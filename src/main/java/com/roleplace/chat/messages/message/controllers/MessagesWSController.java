b3f6494dee0ca689b0cf4a39f60476ba1f91b850
oprosto <90798342+oprosto@users.noreply.github.com>
2026-07-17 22:03:49 +0300

    Добавлено возвращение messageId

--- src/main/java/com/roleplace/chat/messages/message/controllers/MessagesWSController.java
+++ src/main/java/com/roleplace/chat/messages/message/controllers/MessagesWSController.java
@@ -1,6 +1,7 @@
 package com.roleplace.chat.messages.message.controllers;
 
 import com.roleplace.chat.messages.attachments.services.AttachmentService;
+import com.roleplace.chat.messages.message.models.DTO.MessageDTO;
 import com.roleplace.chat.messages.message.models.Message;
 import com.roleplace.chat.messages.message.models.request.MessageRequest;
 import com.roleplace.chat.messages.message.services.MessageService;
@@ -13,19 +14,20 @@ import org.springframework.transaction.annotation.Transactional;
 @Controller
 @RequiredArgsConstructor
 public class MessagesWSController {
+
     private final MessageService messageService;
     private final AttachmentService attachmentService;
     private final SimpMessagingTemplate template;
 
     @Transactional
     @MessageMapping("/receive")
-    public void receiveMessage(MessageRequest messageDTO) {
-        Message message = messageParser(messageDTO);
+    public void receiveMessage(MessageRequest request) {
+        Message message = messageParser(request);
         attachmentService.handleAttachments(message);
-        template.convertAndSend("/topic/messages." + message.getId().getChatId(), messageDTO);
+        template.convertAndSend("/topic/messages." + message.getId().getChatId(), new MessageDTO(message));
     }
 
-    private Message messageParser(MessageRequest messageDTO) {
-        return messageService.createMessage(messageDTO);
+    private Message messageParser(MessageRequest request) {
+        return messageService.createMessage(request);
     }
 }
